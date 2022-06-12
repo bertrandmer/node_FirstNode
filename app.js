43ee6f14e561125002b732b13321f660ae455642
bertrandmer <97544521+bertrandmer@users.noreply.github.com>
2022-06-12 18:20:21 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello World!\n==========\n");
+
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+
+const http = require("http");
+const { Http2ServerResponse } = require("http2");
+
+// Finish setting up the server
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.end("Hello World!");
+})
+.listen(3000, function () {
+    console.log("Server listening...");
+});
